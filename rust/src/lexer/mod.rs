b39ee58e82c551ec0ccd9c583ffe9cdb88478723
August <augustt198@gmail.com>
2014-11-09 23:51:35 -0500

    Update lifetimes

--- rust/src/lexer/mod.rs
+++ rust/src/lexer/mod.rs
@@ -3,13 +3,14 @@ use lexer;
 pub mod yaml;
 
 pub trait Lexer {
-    fn next_token(&self) -> Option<Token>;
+    fn next_token(&mut self) -> Option<Token>;
 }
 
 pub struct Token {
     hi: uint,
     lo: uint,
-    ty: Type
+    ty: Type,
+    src: String
 }
 
 enum Type {
@@ -33,14 +34,17 @@ enum Type {
     StringLit,  // "..."
     Integer,    // [0-9]+
     Decimal,    // [0-9]{0,}.[0-9]+
+    Boolean,    // true|false
     Keyword,    // varies
     Comment,    // varies
 
     Key,
-    Value
+    Value,
+
+    Invalid
 }
 
-trait TokenSource {
+trait TokenSource<'a> {
     fn next_char(&mut self) -> Option<char>;
     fn peak(&self) -> Option<char>;
     fn pos(&self) -> uint;
@@ -51,7 +55,7 @@ struct StringReader {
     pos: uint
 }
 
-impl TokenSource for StringReader {
+impl<'a> TokenSource<'a> for StringReader {
     fn peak(&self) -> Option<char> {
         if self.pos < self.src.len() {
             Some(self.src.char_at(self.pos))
@@ -67,4 +71,18 @@ impl TokenSource for StringReader {
     }
 
     fn pos(&self) -> uint { self.pos }
+}
+
+trait CharUtil {
+    fn is_alpha(self) -> bool;
+    fn is_digit(self) -> bool;
+}
+
+impl CharUtil for char {
+    fn is_alpha(self) -> bool {
+        (self >= 'a' && self <= 'z') || (self >= 'A' && self <= 'Z')
+    }
+    fn is_digit(self) -> bool {
+        self >= '0' && self <= '9'
+    }
 }
--- rust/src/lexer/yaml.rs
+++ rust/src/lexer/yaml.rs
@@ -1,22 +1,68 @@
-use lexer::{TokenSource, Lexer, Token};
+use lexer::{TokenSource, Lexer, Token, CharUtil, Type};
 
 enum YamlState {
     Begin, Val
 }
 
-struct YamlLexer {
+struct YamlLexer<'a> {
     state: YamlState,
-    src: &'static TokenSource + 'static
+    src: &'a mut TokenSource<'a> + 'a
 }
 
-impl YamlLexer {
-    fn new(src: &'static TokenSource + 'static) -> YamlLexer {
+impl<'a> YamlLexer<'a> {
+    fn new(src: &'a mut TokenSource) -> YamlLexer<'a> {
         YamlLexer { src: src, state: Begin }
     }
-}
 
-impl Lexer for YamlLexer {
-    fn next_token(&self) -> Option<Token> {
+    fn next_key(&mut self) -> Option<Token> {
+        let mut string = String::new();
+        let hi = self.src.pos();
+        let mut lo = self.src.pos();
+        loop {
+            let c = match self.src.peak() {
+                Some(chr) => chr,
+                None => return None
+            };
+
+            if c.is_alpha() || c == ':' {
+                string.push(c);
+                self.src.next_char(); // consume peaked char
+                if c == ':' {
+                    break;
+                }
+                lo += 1;
+            } else {
+                break;
+            }
+        }
+        self.state = YamlState::Val;
+        Some(Token {
+            hi: hi,
+            lo: lo,
+            ty: Type::Key,
+            src: string
+        })
+    }
+
+    fn next_val(&mut self) -> Option<Token> {
         None
     }
+}
+
+impl<'a> Lexer for YamlLexer<'a> {
+    fn next_token(&mut self) -> Option<Token> {
+        loop {
+            let c = match self.src.next_char() {
+                Some(chr) => chr,
+                None => return None
+            };
+
+            match self.state {
+                Begin   => return self.next_key(),
+                Val     => return self.next_val()
+            }
+        }
+    }
+
+
 }
